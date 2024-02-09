33e5be9098c1c1ccf0e6b465802bb04e4b4724d0
ilivegod <sekajunior2014@gmail.com>
2024-02-09 23:59:56 +0000

    Hero

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,11 @@
-import Link from "next/link";
-import Image from "next/image";
 import { NAV_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import Button from "./Button";
 
 const Navbar = () => {
   return (
-    <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
         <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
       </Link>
@@ -21,7 +22,22 @@ const Navbar = () => {
         ))}
       </ul>
 
-      <div className="lg:flexCenter hidden"></div>
+      <div className="lg:flexCenter hidden">
+        <Button
+          type="button"
+          title="Login"
+          icon="/user.svg"
+          variant="btn_dark_green"
+        />
+      </div>
+
+      <Image
+        src="menu.svg"
+        alt="menu"
+        width={32}
+        height={32}
+        className="inline-block cursor-pointer lg:hidden"
+      />
     </nav>
   );
 };
